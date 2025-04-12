b4bbd0e659ee8673bbd7f015ddb80c30582b5f87
mohima-mimino11 <mohimahoque22@gmail.com>
2025-04-13 01:00:29 +0600

    more codeforces conditional problems added

--- first_digit.c
+++ first_digit.c
@@ -2,12 +2,14 @@
 int main(){
   int n;
   scanf("%d", &n);
-  n = n / 1000;
-  if(n % 2 == 0){
-    printf("EVEN");
-  }
-  else{
-    printf("ODD");
-  }
+  // n = n / 1000;
+  // if(n % 2 == 0){
+  //   printf("EVEN");
+  // }
+  // else{
+  //   printf("ODD");
+  // }
+  int last_two_digit = n % 100;
+  printf("%d", last_two_digit);
   return 0;
 }
